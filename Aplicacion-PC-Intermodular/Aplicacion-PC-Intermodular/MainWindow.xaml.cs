27337cd4b67314cdc8a00594f888dc70ce48b2d3
AlejandroMartinez02 <martinezmorilloalejandro@gmail.com>
2022-12-23 17:34:32 +0100

    Vista sign in
    La vista del inicio de sesión está prácticamente hecha, falta añadir el registro y los inicios de sesión mediante google

--- Aplicacion-PC-Intermodular/Aplicacion-PC-Intermodular/MainWindow.xaml.cs
+++ Aplicacion-PC-Intermodular/Aplicacion-PC-Intermodular/MainWindow.xaml.cs
@@ -1,17 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
+﻿using System.Windows;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace Aplicacion_PC_Intermodular
 {
@@ -23,6 +11,35 @@ namespace Aplicacion_PC_Intermodular
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Window_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if(e.LeftButton== MouseButtonState.Pressed)
+            {
+                DragMove();
+            }
+        }
+
+        private void minimize_button_Click(object sender, RoutedEventArgs e)
+        {
+            WindowState = WindowState.Minimized;
+        }
+
+        private void close_button_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void btnLogin_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+
+        }
+
+        private void btnLogin_Click_1(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
